8883de6b277970656e07a6508c3a59569901bb85
alek101 <47416711+alek101@users.noreply.github.com>
2021-05-24 11:58:03 +0200

    lesion 2

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,9 @@
 <template>
-  <div class="app">Hello</div>
+  <div class="app">
+    <p>{{name}} - {{ age }} </p>
+    <button @click="changeName('Zelda')">Change Name</button>
+    <button @click="changeAge(30)">Change Age</button>
+  </div>
 </template>
 
 <script lang="ts">
@@ -8,9 +12,21 @@ import { defineComponent } from 'vue';
 
 export default defineComponent({
   name: 'App',
-  components: {
- 
-  }
+  components: {},
+  data() {
+    return {
+      name: 'Link',
+      age: 25 as number | string
+    }
+  },
+    methods: {
+      changeName(name: string) {
+        this.name = name
+      },
+      changeAge(age: number) {
+        this.age = age
+      }
+    }
 });
 </script>
 
